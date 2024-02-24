099a3d5ebabbc40b2178bc560b77a0c9bc274d69
Rishabh <www.majjurocks@gmail.com>
2024-02-24 17:05:29 +0530

    made tailor and appointment responsive

--- components/Tailor.js
+++ components/Tailor.js
@@ -1,13 +1,13 @@
-import { TAILERS, TAILORS } from "@/constants/constants";
+import { TAILERS } from "@/constants/constants";
 import Image from "next/image";
 export default function Tailor() {
   return (
-    <div>
-      <div className="text-centre place-content-center mx-6">
+    <div className="m-10">
+      <div className="text-centre place-content-center">
         <p>
-          <b className="text-4xl my-3">Tailor-made features</b>
+          <b className="text-4xl">Tailor-made features</b>
           <br />
-          <span className="text-2xl my-10 ">
+          <span className="text-2xl">
             Lorem ipsum is common placeholder text used to demonstrate the
             graphic elements of a document or visual presentation.
           </span>
@@ -15,7 +15,7 @@ export default function Tailor() {
       </div>
       <div className="grid grid-cols-3 gap-4 text-black text-center">
         {TAILERS.map((t) => (
-          <div className="mx-9 px-4 place-content-center" key={t.key}>
+          <div className="place-content-center" key={t.key}>
             <Image
               src={t.src}
               width="32"
